177b87c95b7e792d1422bf2cfbf033c75a32741a
TOLIB IBADULLAEV <71664055+TOLIB-prog@users.noreply.github.com>
2023-10-08 16:01:18 +0300

    lesson 0.0.0.2

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 for i in range(1, 10):
     for j in range(1, 10):
-        print(f'{i}* {j}= {j * i:^5}', end="\t")
+        print(f'{j} * {i} = {j * i:^6}', end="\t")
+    print(" ")
+    
+for y in range(1, 10):
+    for g  in range(1, 10):
+        print(f'{y * g:^4}', end="\t")
     print(" ")
